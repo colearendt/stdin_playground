d8611bdd7f490c28354f9e9776543d166d60205b
Cole Arendt <cole.arendt@outlook.com>
2019-10-31 13:50:45 -0400

    show broken sys.stdin

--- sha256_base64_stdin.py
+++ sha256_base64_stdin.py
@@ -9,12 +9,13 @@ print('hi');
 
 #log_file = open("file.txt", "w+")
 log_file = sys.stdout
-#for line in sys.stdin:
-#    print(line, file = log_file)
-#    h = hmac.new('key'.encode('utf8'), line.encode('utf8'), hashlib.sha256).digest()
-#    print(base64.encodebytes(h).decode('utf8'), end = "", file = log_file)
-while True:
-    line = input()
+for line in sys.stdin:
     print(line, file = log_file)
     h = hmac.new('key'.encode('utf8'), line.encode('utf8'), hashlib.sha256).digest()
     print(base64.encodebytes(h).decode('utf8'), end = "", file = log_file)
+
+#while True:
+#    line = input()
+#    print(line, file = log_file)
+#    h = hmac.new('key'.encode('utf8'), line.encode('utf8'), hashlib.sha256).digest()
+#    print(base64.encodebytes(h).decode('utf8'), end = "", file = log_file)
