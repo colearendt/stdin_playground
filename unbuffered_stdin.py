e13b3053cddf4629994a02b57b85793c83a12ccb
Cole Arendt <cole.arendt@outlook.com>
2019-11-19 05:17:17 -0500

    tweak unbuffered_stdin

--- unbuffered_stdin.py
+++ unbuffered_stdin.py
@@ -1,9 +1,19 @@
 import os
 import sys
+import time
 
 # FROM: https://stackoverflow.com/questions/3670323/setting-smaller-buffer-size-for-sys-stdin
 # apparently -u no longer applies to stdin in python3
 
-unbuffered_stdin = os.fdopen(sys.stdin.fileno(), 'rb', buffering=0)
+#unbuffered_stdin = os.fdopen(sys.stdin.fileno(), 'rb', buffering=0)
+#while True:
+#    print(unbuffered_stdin.read(1))
+
 while True:
-    print(unbuffered_stdin.read(1))
+  val = sys.stdin.read(1)
+  if val:
+    print(val)
+  time.sleep(0.05)
+
+# maybe an idea using curses?
+# https://stackoverflow.com/questions/2408560/python-nonblocking-console-input
